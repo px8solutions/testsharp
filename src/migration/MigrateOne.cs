f74e4218dd211ba4f2861911a9bb6a6e20606b54
Destin Hebner <destinhebner@gmail.com>
2016-03-23 19:56:02 -0400

    updated the orms and worked on migrations

---
+++ src/migration/MigrateOne.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentMigrator;
+
+namespace migration
+{
+    [Migration(1)]
+    public class MigrateOne : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("questions")
+                .WithColumn("id").AsInt32().NotNullable().PrimaryKey()
+                .WithColumn("content").AsString().NotNullable()
+                .WithColumn("ordinal").AsInt32().NotNullable()
+                .
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
