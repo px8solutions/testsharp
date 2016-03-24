debd6c57842f04ebc9f01cec6e20f946bf233c2d
Destin Hebner <destinhebner@gmail.com>
2016-03-23 21:38:08 -0400

    migration stuff

--- src/migration/MigrateOne.cs
+++ src/migration/MigrateOne.cs
@@ -16,7 +16,34 @@ namespace migration
                 .WithColumn("id").AsInt32().NotNullable().PrimaryKey()
                 .WithColumn("content").AsString().NotNullable()
                 .WithColumn("ordinal").AsInt32().NotNullable()
-                .
+                .WithColumn("layout_url").AsString().Nullable()
+                .WithColumn("type_id").AsInt32().NotNullable().ForeignKey("question_types", "id")
+                .WithColumn("category_id").AsInt32().NotNullable().ForeignKey("question_categories", "id")
+                .WithColumn("parent_id").AsInt32().Nullable().ForeignKey("questions", "id");
+
+            Create.Table("question_categories")
+                .WithColumn("id").AsInt32().NotNullable().PrimaryKey()
+                .WithColumn("name").AsString().NotNullable();
+
+            Create.Table("question_types")
+                .WithColumn("id").AsInt32().NotNullable().PrimaryKey()
+                .WithColumn("name").AsString().NotNullable();
+
+            Create.Table("responses")
+                .WithColumn("id").AsInt32().NotNullable().PrimaryKey()
+                .WithColumn("content").AsString().NotNullable()
+                .WithColumn("correct").AsBoolean().NotNullable()
+                .WithColumn("ordinal").AsInt32().NotNullable()
+                .WithColumn("question_id").AsInt32().NotNullable().ForeignKey("questions", "id");
+
+            /*
+            CreateTable("fields")
+                .WithColumn("id").AsInt32().NotNullable().PrimaryKey()
+                .WithColumn("x").AsInt16().NotNullable()
+                .WithColumn("y").AsInt16().
+            */
+
+
         }
 
         public override void Down()
