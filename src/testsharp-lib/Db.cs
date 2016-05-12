dfdded5c2572fceeade620716ef95531b4959761
Ethan Reynolds <ethan.reynolds@gmail.com>
2016-05-12 19:09:38 -0400

    added db class

---
+++ src/testsharp-lib/Db.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace testsharp.lib
+{
+    public class Db
+    {
+        private SqlConnection _con;
+        private SqlCommand _cmd;
+        
+        public static String ConnectionString
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["dbcon"].ToString();
+            }
+        }
+
+        public void Open(string connectionString)
+        {
+            _con = new SqlConnection(connectionString);
+            _con.Open();
+        }
+
+        public void Open()
+        {
+            Open(ConnectionString);
+        }
+
+        public System.Data.IDataReader Execute(string sql)
+        {
+            _cmd = _con.CreateCommand();
+            _cmd.CommandText = sql;
+            return _cmd.ExecuteReader();
+        }
+
+        public void Close()
+        {
+            _con.Close();
+        }
+
+
+    }
+}
---
+++ src/testsharp/Controllers/NothingController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using testsharp.lib;
+
+namespace testsharp.Controllers
+{
+    public class NothingController : Controller
+    {
+        // GET: Nothing
+        public ActionResult Nothing()
+        {
+
+            Db db = new Db();
+            db.Open();
+
+            var reader = db.Execute("select * from questions where id in (2,3,4)");
+
+            while (reader.Read())
+            {
+                ViewBag.blah += reader["id"] +"; " ;
+            }
+
+            reader.Close();
+            db.Close();
+
+            ViewBag.db = testsharp.lib.Db.ConnectionString;
+            return View();
+        }
+    }
+}
