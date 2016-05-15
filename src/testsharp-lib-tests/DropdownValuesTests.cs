a54e9287b48f3e7d6471dbc54bba332310ab0b56
Destin Hebner <destinhebner@gmail.com>
2016-05-14 20:44:08 -0400

    testing

---
+++ src/testsharp-lib-tests/DropdownValuesTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using testsharp.lib;
+
+namespace testsharp_lib_tests
+{
+    [TestFixture]
+    public class DropdownValuesTests
+    {
+        [Test]
+        public void Load()
+        {
+            DropdownValue dv1 = DropdownValue.Load(0);
+
+            Assert.AreEqual(dv1.id, 0);
+            Assert.AreEqual(dv1.content, "fix the jqueries");
+            Assert.AreEqual(dv1.fieldId, 2);
+
+            DropdownValue dv2 = DropdownValue.Load(1);
+
+            Assert.AreEqual(dv2.id, 1);
+            Assert.AreEqual(dv2.content, "Kroger Brand Seltzer Water");
+            Assert.AreEqual(dv2.fieldId, 2);
+        }
+    }
+}
---
+++ src/testsharp-lib-tests/FieldsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using testsharp.lib;
+
+namespace testsharp_lib_tests
+{
+    [TestFixture]
+    public class FieldsTests
+    {
+        [Test]
+        public void Load()
+        {
+            Field field = Field.Load(1);
+
+            Assert.AreEqual(field.id, 1);
+            Assert.AreEqual(field.x, 21);
+            Assert.AreEqual(field.x, 32);
+            Assert.AreEqual(field.x, 30);
+            Assert.AreEqual(field.x, 10);
+            Assert.AreEqual(field.Response, Response.Load(6));
+            Assert.AreEqual(field.FieldType, 0);
+        }
+    }
+}
